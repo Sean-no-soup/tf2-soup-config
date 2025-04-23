016ecbd451061f9bae8ce01ab36545be67e98fa5
Soup, no Sean <67440917+Sean-no-soup@users.noreply.github.com>
2025-04-23 01:54:44 -0400

    Update inventory count.py
    better grouping and sorting, more readable output

--- inventory count.py
+++ inventory count.py
@@ -1,26 +1,82 @@
 #importing multiline inputs without lagging out python and being intuitive
 #turned out to be much more annoying than expected
-#get a count of identical items from the inventorymarkdown backpack.tf gives (for the eyelander collection)
+import re
 
+print("starting")
+
+output = open("tf2_inv_output.txt","w")
+output.close()
 with open('inventory markdown.txt','r') as file:content = file.read()
-itemlist = content.splitlines()
+
+invlist = content.splitlines()
 groupedlist = []
+count = 0
 
-#I coud do this with a class but lazy. might have to if I want to do more with this info like graphs. could extract a bunch of data directly from backpack with the other markdown options
-for item in itemlist:
-    for index in range(len(groupedlist)):
-        group = groupedlist[index][0]
-        if group == item:
-            groupedlist[index][1] += 1
-            break
+for invstring in invlist: #parse
+
+    count += 1
+    if count % 50 == 0:print(".",end = "")
+
+    item = re.findall(r"^.*?(?=\s\()",  invstring) #before " ("
+    item = item[0] if item else invstring
+
+    context = (re.findall(r"\((.*?)\)", invstring)) #between parentheses
+    context = context[0].replace("â€“","-") if context else ""
+
+    cost_unit = re.findall(r"(key|\$|ref)", context)
+    cost_unit = cost_unit[0] if cost_unit else ""
+
+    cost_list = re.findall(r"[0-9\.]+", context)
+    if cost_list:
+        cost_min, cost_max = cost_list[0],cost_list[-1]
     else:
-        groupedlist += [[item,1]]
+        cost_min, cost_max = "",""
 
-#sort by number cuz eyelanders
+    warpaint_wear = re.findall(r"(Factory New|Minimal Wear|Field-Tested|Well-Worn|Battle-Scarred)", context)
+    warpaint_wear = warpaint_wear[0] if warpaint_wear else""
+
+
+    for group in groupedlist: #group
+        if group[0]:
+            if item == group[0]:
+                group[1] += 1
+                break
+    else:
+        groupedlist += [[item, 1, cost_unit, cost_min, cost_max, warpaint_wear]]
+
+    item = ['']
+    cost_unit = ['']
+    cost_min = ['']
+    cost_max = ['']
+    warpaint_wear = ['']
+
+
+#sort by item name
+groupedlist.sort(key = lambda sublist: sublist[0])
+
+#sort by count
 groupedlist.sort(reverse = True, key = lambda sublist: sublist[1])
-total = 0
-for group,count in groupedlist:
-    total += count
-    print(group,count)
+
+#sort by cost_min
+groupedlist.sort(reverse = True, key = lambda sublist: (float(sublist[3]))if sublist[3] else 0 )
+
+#sort by cost_unit
+groupedlist.sort(key = lambda sublist: sublist[2])
+
+output = open("tf2_inv_output.txt","a")
+for group in groupedlist:
+
     
-print('sanity check, total inventory slots used:',total)
+    output.write(group[3].ljust(7))#cost_min
+    output.write(group[2].ljust(5))#cost_unit
+
+    output.write("x   ")
+
+    output.write(str(group[1]).ljust(6))#count
+    output.write(group[0].ljust(30))#item
+
+    output.write("\n")
+    
+output.close()
+print("\ndone")
+print("sanity check, total inventory slots used:", count)
